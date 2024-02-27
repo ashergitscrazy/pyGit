fd58070873d6938417e736d29dd1380d29070d43
ashergitscrazy <asher.jae@gmail.com>
2024-02-26 22:44:10 -0500

    physics updates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Template pygame setup from replit
 import sys
 import random
+import perlin_noise
+from perlin_noise import PerlinNoise
 
 import pygame
 from pygame.locals import QUIT
@@ -14,11 +16,16 @@ clock = pygame.time.Clock()
 
 walk_frames = ["idle.png", "1.png", "2.png", "3.png", "4.png"]
 BLOCK_WIDTH_HEIGHT = 50
-SEA_LEVEL = 341
-WORLD_WIDTH = 20
-WORLD_DEPTH = 10
-player_x = (369,431)
-player_y = 0
+SEA_LEVEL = 340
+WORLD_WIDTH = 40
+WORLD_DEPTH = 20
+NOISE_CONSTANT = 10
+PLAYER_HITBOX = pygame.Rect(380, 259, 42, 81)
+GRAVITY = 1
+TERMINAL_VELOCITY = -20
+JUMP_HEIGHT = 20
+Y_VELOCITY = JUMP_HEIGHT
+jumping = False
 
 # some comment I've added to test git
 
@@ -28,18 +35,21 @@ class Player():
         self.tick = 5
         self.frame = 0
         self.dir = 1
-        self.vel = 0
+        self.vel_y = 0
         self.accel = 5
+        self.floor = 0
         self.img = pygame.image.load("idle.png")
-        self.hitbox = pygame.Rect(369, 259, 62, 82)
+        self.rect = PLAYER_HITBOX
 
     def draw(self):
+        global jumping, terrain_y
         screen.blit(self.img, (340, 240))
-        # 340, 240
-
+        pygame.draw.rect(screen, 33333333, PLAYER_HITBOX, 2)
     def move(self):
-        global player_x, player_y
+        global jumping, falling
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and not jumping:
+            jumping = True
         if keys[pygame.K_a] or keys[pygame.K_d]:
             if self.tick == 0:
                 self.frame = 1 + (self.frame) % 4
@@ -60,7 +70,25 @@ class Player():
                 self.img = pygame.transform.flip(self.img, True, False)
             return 0
 
+    def find_floor(self):
+        finder_depth = 5
+        searching_for_floor = True
+        self.floor = 0
+        while searching_for_floor:
+            floor_finder = pygame.Rect(PLAYER_HITBOX.x, PLAYER_HITBOX.y, PLAYER_HITBOX.width, finder_depth)
+            for column in terrain.blocks:
+                for block in column:
+                    if block.rect.colliderect(floor_finder):
+                        floor = block.is_floor()
+                        return floor
+            finder_depth += 50
+            if finder_depth > 10000:
+                searching_for_floor = False
+
+
 player = Player()
+
+
 # player x dimension: 60
 # player y dimension: 80
 
@@ -71,50 +99,84 @@ class Block():
         self.img = pygame.image.load(blockType + ".png")
         self.img = pygame.transform.scale(self.img, (BLOCK_WIDTH_HEIGHT, BLOCK_WIDTH_HEIGHT))
         self.pos = pos
-        self.draw(0)
+        self.draw(0,0)
 
-    def draw(self, x_pos):
-        screen.blit(self.img, (self.pos["x"] * BLOCK_WIDTH_HEIGHT - x_pos, self.pos["y"] * BLOCK_WIDTH_HEIGHT + SEA_LEVEL))
+    def draw(self, x_pos, y_pos):
+        self.location = ((self.pos["x"] * BLOCK_WIDTH_HEIGHT) - x_pos, (self.pos["y"] * BLOCK_WIDTH_HEIGHT) + SEA_LEVEL - y_pos)
+        screen.blit(self.img, self.location)
+        self.rect = pygame.Rect(self.location[0], self.location[1], BLOCK_WIDTH_HEIGHT, BLOCK_WIDTH_HEIGHT)
+        pygame.draw.rect(screen, 000000, self.rect, 2)
+        self.y = y_pos
+
     def destroy(self):
-        self.img = pygame.image.load("air.png")
-        self.destroyed = True
+        if self.destroyed == False:
+            self.img = pygame.image.load("air.png")
+            self.destroyed = True
+
+    def is_floor(self):
+        return self.rect
 
 
-
-class Terrain():
-    def __init__(self, width, depth, block):
+class Terrain:
+    def __init__(self, width, depth):
         self.blocks = []
-        for d in range(depth):
-            row = []
-            for w in range(width):
-                row.append(Block(block, {"x": w, "y": d}))
-            self.blocks.append(row)
+        for w in range(width):
+            column = []
+            dirt_thickness = random.randint(3, 4)
+            for d in range(depth[w]):
+                height = WORLD_DEPTH - d
+                if d - depth[w] == -1:
+                    column.append(Block("grass", {"x": w, "y": height}))
+                elif d >= depth[w] - dirt_thickness:
+                    column.append(Block("dirt", {"x": w, "y": height}))
+                else:
+                    column.append(Block("stone", {"x": w, "y": height}))
+            self.blocks.append(column)
 
-    def draw(self, x):
-        for row in self.blocks:
-            for block in row:
-                block.draw(x)
+    def draw(self, x, y):
+        for column in self.blocks:
+            for block in column:
+                block.draw(x, y)
 
 
 
-terrain = Terrain(WORLD_WIDTH, WORLD_DEPTH, "dirt")
+
+# Terrain generation shenanigans
+
+seed = random.randint(0, 20000)
+noise = PerlinNoise(octaves=3, seed=seed)
+height_map = []
+for i in range(WORLD_WIDTH):
+    height = NOISE_CONSTANT * noise([i / WORLD_WIDTH])
+    height = round(height)
+    height_map.append(WORLD_DEPTH + height)
+
+terrain = Terrain(WORLD_WIDTH, height_map)
 
 terrain_x = 0
-tick = 5
+terrain_y = -30
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    dir = player.move()
-    if dir:
-        terrain_x += 10 * dir
-        tick = 2
-    if tick > 0:
-        screen.fill(bg_color)
-        terrain.draw(terrain_x)
-        tick -= 1
+    move_dir = player.move()
+    floor_block = player.find_floor()
+    floor_collision = floor_block.colliderect(PLAYER_HITBOX)
+    if abs(floor_block.top - PLAYER_HITBOX.bottom) > 3:
+        if not floor_collision and not jumping:
+            terrain_y += GRAVITY * 5
+    if jumping:
+        terrain_y -= Y_VELOCITY
+        Y_VELOCITY -= GRAVITY
+    if floor_collision:
+        jumping = False
+        Y_VELOCITY = JUMP_HEIGHT
+        terrain_y -= 10
+    if move_dir:
+        terrain_x += 10 * move_dir
+    screen.fill(bg_color)
+    terrain.draw(terrain_x, terrain_y)
     player.draw()
     pygame.display.flip()
     clock.tick(60)
-
